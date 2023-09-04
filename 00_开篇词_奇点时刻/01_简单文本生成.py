c826767b1bf01aef6f01df9be43db35be616b744
Huang Jia <huangj2@ihpc.a-star.edu.sg>
2023-09-04 21:22:31 +0800

    Initial commit

---
+++ 00_开篇词_奇点时刻/01_简单文本生成.py
@@ -0,0 +1,6 @@
+import os
+os.environ["OPENAI_API_KEY"] = '你的Open AI API Key'
+from langchain.llms import OpenAI
+llm = OpenAI(model_name="text-davinci-003",max_tokens=200)
+text = llm("请给我写一句情人节红玫瑰的中文宣传语")
+print(text)
---
+++ 00_开篇词_奇点时刻/02_看图说话.py
@@ -0,0 +1,60 @@
+#---- Part 0 导入所需要的类
+import os
+import requests
+from PIL import Image
+import torch
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from langchain.tools import BaseTool
+from langchain import OpenAI
+from langchain.agents import initialize_agent, AgentType
+
+import os
+os.environ["OPENAI_API_KEY"] = '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'
+
+#---- Part I 初始化图像字幕生成模型
+# 指定要使用的工具模型（HuggingFace中的image-caption模型）
+hf_model = "Salesforce/blip-image-captioning-large"
+# 如果可用，使用GPU
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# 初始化处理器和工具模型
+# 预处理器将准备图像供模型使用
+processor = BlipProcessor.from_pretrained(hf_model)
+# 然后我们初始化工具模型本身
+model = BlipForConditionalGeneration.from_pretrained(hf_model).to(device)
+
+#---- Part II 定义图像字幕生成工具类
+class ImageCapTool(BaseTool):
+   
+    name = "Image captioner"
+    description = "为图片创作说明文案."
+
+    def _run(self, url: str):
+        # 下载图像并将其转换为PIL对象
+        image = Image.open(requests.get(url, stream=True).raw).convert('RGB')
+        # 预处理图像
+        inputs = processor(image, return_tensors="pt").to(device)
+        # 生成字幕
+        out = model.generate(**inputs, max_new_tokens=20)
+        # 获取字幕
+        caption = processor.decode(out[0], skip_special_tokens=True)
+        return caption
+    
+    def _arun(self, query: str):
+        raise NotImplementedError("This tool does not support async")
+
+#---- PartIII 初始化并运行LangChain智能体
+# 设置OpenAI的API密钥并初始化大语言模型（OpenAI的Text模型）
+os.environ["OPENAI_API_KEY"] = '你的OpenAI API Key'
+llm = OpenAI(temperature=0.5)
+
+# 使用工具初始化智能体并运行
+tools = [ImageCapTool()]
+agent = initialize_agent(
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    tools=tools,
+    llm=llm,
+    verbose=True,
+)
+img_url = 'https://mir-s3-cdn-cf.behance.net/project_modules/hd/eec79e20058499.563190744f903.jpg'
+agent.run(input=f"请创作合适的中文推广文案?\n{img_url}")
