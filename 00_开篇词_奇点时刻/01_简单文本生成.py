b1348b98d4462558ad9840e45f3580b49cbf6ff9
Huang Jia <huangj2@ihpc.a-star.edu.sg>
2024-01-08 23:31:03 +0800

    202401大家好！

--- 00_开篇词_奇点时刻/01_简单文本生成.py
+++ 00_开篇词_奇点时刻/01_简单文本生成.py
@@ -2,8 +2,11 @@
 https://time.geekbang.org/column/intro/100617601
 作者 黄佳'''
 
-import os
-os.environ["OPENAI_API_KEY"] = '你的Open AI API Key'
+from dotenv import load_dotenv  # 用于加载环境变量
+load_dotenv()  # 加载 .env 文件中的环境变量
+
+# import os
+# os.environ["OPENAI_API_KEY"] = '你的Open AI API Key'
 from langchain.llms import OpenAI
 llm = OpenAI(model_name="text-davinci-003",max_tokens=200)
 text = llm("请给我写一句情人节红玫瑰的中文宣传语")
--- 02_文档QA系统/DocQA.py
+++ 02_文档QA系统/DocQA.py
@@ -3,7 +3,9 @@ https://time.geekbang.org/column/intro/100617601
 作者 黄佳'''
 
 import os
-os.environ["OPENAI_API_KEY"] = '你的OpenAI API Key'
+# os.environ["OPENAI_API_KEY"] = '你的OpenAI API Key'
+from dotenv import load_dotenv  # 用于加载环境变量
+load_dotenv()  # 加载 .env 文件中的环境变量
 
 # 1.Load 导入Document Loaders
 from langchain.document_loaders import PyPDFLoader
